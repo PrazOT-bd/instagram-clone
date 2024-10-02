6d997aad8402a7427ff047e568e9862b4ad13635
PrazOT <praiseotoro@gmail.com>
2024-10-02 11:26:20 +0100

    worked on the sidebar function

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,40 +1,57 @@
 import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
-import { useState } from "react";
+import useFollowUser from "../../hooks/useFollowUser";
+import useAuthStore from "../../store/authStore";
 
 
-const SuggestedUser = ({followers, name, avatar}) => {
-    const [isFollowed, setIsFollowed] = useState(false);
-  return <>
-    <Flex alignItems={"center"} justifyContent={"space-between"} mt={4} w={"full"}>
-        <Flex alignItems={"center"} gap={2}>
-            <Avatar src={avatar} size={"md"} />
-            <VStack spacing={2}>
-                <Box fontSize={12} fontWeight={"bold"}>
-                    {name}
-                </Box>
-                <Box fontSize={12} color={"gray.500"}>
-                    {followers} followers
-                </Box>
-            </VStack>
+
+const SuggestedUser = ({ user, setUser }) => {
+    const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(user.uid);
+    const authUser = useAuthStore((state) => state.user);
+
+    const onFollowUser = async () => {
+        await handleFollowUser();
+        setUser({
+            ...user,
+            followers: isFollowing
+                ? user.followers.filter((follower) => follower.uid !== authUser.uid)
+                : [...user.followers, authUser],
+        });
+    };
+
+    return <>
+        <Flex alignItems={"center"} justifyContent={"space-between"} mt={4} w={"full"}>
+            <Flex alignItems={"center"} gap={2}>
+                <Avatar src={user.profilePicURL} size={"md"} />
+                <VStack spacing={2}>
+                    <Box fontSize={12} fontWeight={"bold"}>
+                        {user.fullName}
+                    </Box>
+                    <Box fontSize={12} color={"gray.500"}>
+                        {user.followers.length} followers
+                    </Box>
+                </VStack>
+            </Flex>
+
+            {authUser.uid !== user.uid && (
+                <Button
+                    fontSize={13}
+                    fontWeight={"medium"}
+                    color={"blue.500"}
+                    bg={"transparent"}
+                    _hover={{ color: "white" }}
+                    p={0} h={"max-content"}
+                    cursor={"pointer"}
+                    onClick={onFollowUser}
+                    isLoading={isUpdating}
+                >
+                    {isFollowing ? "Unfollow" : "Follow"}
+                </Button>
+            )}
         </Flex>
-        
-        <Button
-        fontSize={13}
-        fontWeight={"medium"}
-        color={"blue.500"}
-        bg={"transparent"}
-        _hover={{color: "white"}}
-        p={0} h={"max-content"}
-        cursor={"pointer"}
-        onClick={() => setIsFollowed(!isFollowed)}
-        >
-            {isFollowed ? "Unfollow" : "Follow" }
-        </Button>
-    </Flex>
-  
-  
-  
-  </>
+
+
+
+    </>
 };
 
 export default SuggestedUser;
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,45 +1,41 @@
-import { Box, Flex, Text, VStack, Link } from "@chakra-ui/react"
-import SuggestedHeader from "./SuggestedHeader"
-import SuggestedUser from "./SuggestedUser"
-// import { Link } from "react-router-dom"
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
+import SuggestedHeader from "./SuggestedHeader";
+import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 const SuggestedUsers = () => {
-  return <>
-    <VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
+    const { isLoading, suggestedUsers } = useGetSuggestedUsers();
 
-        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>Suggested for you</Text>
-            <Text fontSize={12} fontWeight={"bold"} color={"blue.500"} cursor={"pointer"}>See All</Text>
-        </Flex>
 
-        <SuggestedUser name='Dan Abramov' followers={1345} avatar='https://bit.ly/dan-abramov' />
-        <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
-        <SuggestedUser name='Christian Nwamba' followers={876} avatar='https://bit.ly/code-beast'/>
+    if (isLoading) return null;
 
-        <Box
-        fontSize={12}
-        color={"gray.500"}
-        mt={5}
-        >
-            © 2024 Built By {" "}
-            <Link href="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" target={"_blank"} color={"blue.500"} fontWeight={"bold"}>Praise</Link>
-        </Box>
+    return (
+        <VStack py={8} px={6} gap={4}>
+            <SuggestedHeader />
 
-        {/* <Flex alignItems={"center"} gap={4}>
-            <img src="XXXXXXXXXXXXXXXXXXXXXXXXXX" alt="user" className="h-12 w-12 rounded-full object-cover" />
-            <Flex flexDir={"column"}>
-                <h3 className="font-bold text-sm">username</h3>
-                <p className="text-sm text-gray-500">Followed by someone + more</p>
-            </Flex>
-            <a href="#">Follow</a>
-        </Flex> */}
-    
+            {suggestedUsers.length !== 0 && (
+                <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+                    <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+                        Suggested for you
+                    </Text>
+                    <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+                        See All
+                    </Text>
+                </Flex>
+            )}
 
-    </VStack>
-  
-  
-  </>
-}
+            {suggestedUsers.map((user) => (
+                <SuggestedUser user={user} key={user.id} />
+            ))}
 
-export default SuggestedUsers
+            <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+                © 2024 Built By{" "}
+                <Link href='https://github.com/PrazOT-bd' target='_blank' color='blue.500' fontSize={14}>
+                    Praise
+                </Link>
+            </Box>
+        </VStack>
+    );
+};
+
+export default SuggestedUsers;
